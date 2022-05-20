69cb8031a5e4223707442205966f0561c66de372
JoshuahBoardman <joshuahboardman@gmail.com>
2022-05-20 17:27:51 -0500

    Added a game over menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,68 @@
 import { invaderDraw, invaderUpdate, allInvaders } from "./invader.js";
-import { playerDraw, playerUpdate, player} from "./player.js";
+import { playerDraw, playerUpdate, player } from "./player.js";
 import { sharePosition } from "./grid.js";
 import { projectileDraw, projectileUpdate } from "./projectile.js";
 import { displayScore } from "./score.js";
 
-
 const grid = document.getElementById("grid");
+const menu = document.getElementById("game-menu");
 
 const GAME_SPEED = 7;
 let previousTime = 0;
 
 function gameLoop(timeStamp) {
-    let requestId = window.requestAnimationFrame(gameLoop);
-    let delta = (timeStamp - previousTime) / 1000;
-    if(delta < 1 / GAME_SPEED) return
-    
-    previousTime = timeStamp;
+  let requestId = window.requestAnimationFrame(gameLoop);
+  let delta = (timeStamp - previousTime) / 1000;
+  if (delta < 1 / GAME_SPEED) return;
 
-    update();
-    draw();
-    displayScore();
-    gameOver(requestId);
-    win(requestId);
-};
+  previousTime = timeStamp;
+
+  update();
+  draw();
+  displayScore();
+  gameOver(requestId);
+  win(requestId);
+}
 
 function draw() {
-    grid.innerHTML = ""
-    playerDraw(grid);
-    invaderDraw(grid);
-    projectileDraw(grid);
-};
+  grid.innerHTML = "";
+  playerDraw(grid);
+  invaderDraw(grid);
+  projectileDraw(grid);
+}
 
 function update() {
-    playerUpdate();
-    invaderUpdate();
-    projectileUpdate();
-};
+  playerUpdate();
+  invaderUpdate();
+  projectileUpdate();
+}
 
 function isGameOver() {
-    return allInvaders.some(invader => {
-        return sharePosition(invader, player) || invader.y > player.y;
-    });
-};
+  return allInvaders.some((invader) => {
+    return sharePosition(invader, player) || invader.y > player.y;
+  });
+}
 
 function gameOver(id) {
-    if (!isGameOver()) return;
-    window.cancelAnimationFrame(id);
-    // TODO set a time delay before and add replay button
-    grid.innerHTML = "";
-    displayResultText("You Lose");
-};
+  if (!isGameOver()) return;
+  window.cancelAnimationFrame(id);
+  // TODO set a time delay before and add replay button
+  grid.innerHTML = "";
+  displayResultText("You Lose");
+}
 
 function win(id) {
-    if(allInvaders.length !== 0) return;
-    window.cancelAnimationFrame(id);
-    grid.innerHTML = "";
-    displayResultText("You Win!");
+  if (allInvaders.length !== 0) return;
+  window.cancelAnimationFrame(id);
+  grid.innerHTML = "";
+  displayResultText("You Win!");
 }
 
 function displayResultText(string) {
-    const resultText = document.createElement("div");
-    resultText.classList.add("result-text");
-    resultText.innerText = `${string}`;
-    grid.appendChild(resultText);
+  menu.innerHTML = `
+    <h2 class="game-over-menu-header">${string}</h2>
+    <a href="/" class="game-over-button">Play Again</a>
+    `;
 }
 
 window.requestAnimationFrame(gameLoop);
